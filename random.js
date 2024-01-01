5b198f41fccd29c0aa39f47a1f270aa05156a149
foxtrot5f3s_hideout <141502127+Thecartoondude@users.noreply.github.com>
2023-12-31 20:11:04 -0500

    Update random.js

--- random.js
+++ random.js
@@ -1,4 +1,12 @@
-function accessrandompage() {
-var randompage = Math.floor(Math.random() * diary.length);
-window.location.replace("index" + randompage + ".html");
+add_action('template_redirect', 'wpsf_random_page_redirect');
+function wpsf_random_page_redirect()
+{
+	if (is_page('random-page')) {
+		$excluded_pages = array(15, 9);
+		$pages = get_pages(array('exclude' => $excluded_pages));
+		$random_page = array_rand($pages);
+		$page_url = get_page_link($random_page);
+		wp_redirect($page_url);
+		exit;
+	}
 }
